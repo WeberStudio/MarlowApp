e78dc30ae552d3210f346309894a2c4a811eaa40
Shahid <elusive.aries@gmail.com>
2013-09-18 09:50:10 +0500

    new chnges

--- app/controller/DashboardC.js
+++ app/controller/DashboardC.js
@@ -5,7 +5,7 @@ Ext.define('MarlowApp.controller.DashboardC', {
     config: {
         models: ['all_products', 'Shops_Model', 'Save_User_SelectionM'],         
         stores: ['all_products', 'Shops_Store', 'Save_User_SelectionS'],         
-        views : ['Dashboard', 'Snap', 'Shops', 'MyItemList', 'AddNote', 'AddToList' ,'DeleteItem'],     
+        views : ['Dashboard', 'Snap', 'Shops', 'MyItemList', 'AddNote', 'AddToList' ,'DeleteItem', 'ShareIndivisual', 'ShareItem', 'ConfirmDel'],     
         refs: {
            
             saveNoteId:    '#saveNoteId',
